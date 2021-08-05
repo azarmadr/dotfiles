4d977393ca37d2cc1a78b47a3447817754127135
Azhar Madar Shaik <azarshaik454@gmail.com>
2021-08-05 12:14:27 +0530

    Add dot_config/wezterm/wezterm.lua

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm';
+
+wezterm.on("format-window-title", function(tab, pane, tabs, panes, config)
+  local zoomed = ""
+  if tab.active_pane.is_zoomed then
+    zoomed = "[Z] "
+  end
+
+  local index = ""
+  if #tabs > 1 then
+    index = string.format("[%d/%d] ", tab.tab_index + 1, #tabs)
+  end
+
+  return zoomed .. index .. tab.active_pane.title
+end)
+
+return {
+  default_prog = {'F:/@win/scoop/apps/pwsh/current/pwsh.exe'},
+  font = wezterm.font("JetBrains Mono"),
+  color_scheme = "Batman",
+}
+
